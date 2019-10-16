81052274c0c90532ff3ac67e35c29e71f5a2297b
hsalamat <hooman_salamat@yahoo.com>
2019-10-16 10:55:24 -0400

    Update d3dApp.cpp

--- Common/d3dApp.cpp
+++ Common/d3dApp.cpp
@@ -437,8 +437,8 @@ bool D3DApp::InitDirect3D()
 	// Try to create hardware device.
 	HRESULT hardwareResult = D3D12CreateDevice(
 		//nullptr,             // default adapter
-		adapterList[1],          //NVDIA adapter! at home, you can comment this out and use nullptr to point to a stronger adapter
-		D3D_FEATURE_LEVEL_12_0,
+		adapterList[0],          //NVDIA adapter! at home, you can comment this out and use nullptr to point to a stronger adapter
+		D3D_FEATURE_LEVEL_11_0,
 		IID_PPV_ARGS(&md3dDevice));
 
 
