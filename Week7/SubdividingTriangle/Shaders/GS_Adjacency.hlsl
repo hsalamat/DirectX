4e3f70ecad00aecc78a10f86209c62a9d6484916
hsalamat <hooman.salamat@yahoo.com>
2019-10-18 14:29:11 -0400

    updated

---
+++ Week7/SubdividingTriangle/Shaders/GS_Adjacency.hlsl
@@ -0,0 +1,74 @@
+cbuffer cbPerObject : register(b0)
+{
+    float4x4 gWorldViewProj;
+};
+
+
+
+struct VertexOut
+{
+    float3 PosW : POSITION;
+    float4 Color : COLOR;
+};
+
+struct GeoOut
+{
+    float4 PosH : SV_POSITION;
+    float3 PosW : POSITION;
+    float4 Color : COLOR;
+    uint PrimID : SV_PrimitiveID;
+};
+
+
+
+[maxvertexcount(12)]
+void GS(triangleadj VertexOut gin[6], //PrimitiveType InputVertexType InputName[NumElements]-> one vertex for a point, two for aline, threefor atriangle, four for line with adjacency, and six for a triangle with adjacency.
+        //uint primID : SV_PrimitiveID, //inout StreamOutputObject<OutputVertexType>
+        inout TriangleStream<GeoOut> triStream)  //OutputName
+{
+
+    GeoOut gout[6];
+    [unroll]
+    for (uint i = 0; i < 6; i=i+2)
+    {
+        // Transform to homogeneous clip space.
+        gout[i].PosH = mul(float4(gin[i].PosW, 1.0f), gWorldViewProj);
+        gout[i].Color = gin[i].Color;
+        triStream.Append(gout[i]);
+    }
+
+    triStream.RestartStrip();
+
+    for (uint i = 0; i < 3; i ++)
+    {
+        // Transform to homogeneous clip space.
+        gout[i].PosH = mul(float4(gin[i].PosW, 1.0f), gWorldViewProj);
+        gout[i].Color = gin[i].Color;
+        triStream.Append(gout[i]);
+    }
+    
+    triStream.RestartStrip();
+
+    for (uint i = 2; i < 5; i++)
+    {
+        // Transform to homogeneous clip space.
+        gout[i].PosH = mul(float4(gin[i].PosW, 1.0f), gWorldViewProj);
+        gout[i].Color = gin[i].Color;
+        triStream.Append(gout[i]);
+    }
+    
+    triStream.RestartStrip();
+
+    for (uint i = 4; i <= 6; i++)
+    {
+        if (i == 6)
+            i = 0;
+        // Transform to homogeneous clip space.
+        gout[i].PosH = mul(float4(gin[i].PosW, 1.0f), gWorldViewProj);
+        gout[i].Color = gin[i].Color;
+        triStream.Append(gout[i]);
+    }
+    
+    triStream.RestartStrip();
+
+}
