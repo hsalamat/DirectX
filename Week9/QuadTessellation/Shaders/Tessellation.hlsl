fa3c8f0e58c2ccaa405713d989afb9fd3b14122d
hsalamat <hooman.salamat@yahoo.com>
2019-10-25 15:23:17 -0400

    updated

---
+++ Week9/QuadTessellation/Shaders/Tessellation.hlsl
@@ -0,0 +1,87 @@
+//***************************************************************************************
+// Tessellation.hlsl 
+//***************************************************************************************
+
+#include "VS.hlsl"
+
+
+
+struct PatchTess
+{
+	float EdgeTess[4]   : SV_TessFactor;
+	float InsideTess[2] : SV_InsideTessFactor;
+};
+
+PatchTess ConstantHS(InputPatch<VertexOut, 4> patch, uint patchID : SV_PrimitiveID)
+{
+	PatchTess pt;
+	
+    // Uniformly tessellate the patch 3 times.
+    pt.EdgeTess[0] = 2; // Left edge
+    pt.EdgeTess[1] = 2; // Top edge
+    pt.EdgeTess[2] = 2; // Right edge
+    pt.EdgeTess[3] = 2; // Bottom edge
+    pt.InsideTess[0] = 3; // u-axis (columns)
+    pt.InsideTess[1] = 3; // v-axis (rows)
+    return pt;
+
+	
+	return pt;
+}
+
+struct HullOut
+{
+	float3 PosL : POSITION;
+    float4 Color : COLOR;
+};
+
+[domain("quad")]
+[partitioning("integer")]
+[outputtopology("triangle_cw")]
+[outputcontrolpoints(4)]
+[patchconstantfunc("ConstantHS")]
+[maxtessfactor(64.0f)]
+HullOut HS(InputPatch<VertexOut, 4> p, 
+           uint i : SV_OutputControlPointID,
+           uint patchId : SV_PrimitiveID)
+{
+	HullOut hout;
+	
+	hout.PosL = p[i].PosL;
+    hout.Color = p[i].Color;
+	
+	return hout;
+}
+
+struct DomainOut
+{
+	float4 PosH : SV_POSITION;
+    float4 Color : Color;
+};
+
+// The domain shader is called for every vertex created by the tessellator.  
+// It is like the vertex shader after tessellation.
+[domain("quad")]
+DomainOut DS(PatchTess patchTess, 
+             float2 uv : SV_DomainLocation, 
+             const OutputPatch<HullOut, 4> quad)
+{
+	DomainOut dout;
+	
+	// Bilinear interpolation.
+	float3 v1 = lerp(quad[0].PosL, quad[1].PosL, uv.x); 
+	float3 v2 = lerp(quad[2].PosL, quad[3].PosL, uv.x); 
+	float3 p  = lerp(v1, v2, uv.y); 
+	
+	// Displacement mapping
+	//p.y = 0.3f*( p.z*sin(p.x) + p.x*cos(p.z) );
+	
+	float4 posW = mul(float4(p, 1.0f), gWorld);
+	dout.PosH = mul(posW, gViewProj);
+    dout.Color = quad[0].Color;
+	return dout;
+}
+
+
+
+
