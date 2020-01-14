a4ba21707727aedd70e8a55f8994a3551ee9992e
hsalamat <hooman_salamat@yahoo.com>
2020-01-14 17:06:57 -0500

    updated

--- Week3/Week3-1-TriangleStepByStep/Triangle9-cubewith6 colors.cpp
+++ Week3/Week3-1-TriangleStepByStep/Triangle9-cubewith6 colors.cpp
@@ -1,5 +1,5 @@
 //***************************************************************************************
-// Use constant buffer to do rotation
+// Cube with different face colors
 //***************************************************************************************
 
 #include "../../Common/d3dApp.h"
@@ -14,7 +14,7 @@ using namespace DirectX::PackedVector;
 struct Vertex
 {
 	XMFLOAT3 Pos;
-	XMFLOAT4 Color;
+	//XMFLOAT4 Color;
 };
 
 
@@ -24,13 +24,35 @@ struct ObjectConstants
 };
 
 
-class TriangleAPP : public D3DApp
+struct ObjectConstant2
+{
+	struct {
+		float r;
+		float g;
+		float b;
+		float a;
+	} face_colors[6];
+};
+
+const ObjectConstant2 cb2 =
+{
+	{
+		{1.0f, 0.0f, 1.0f, 1.0f },
+		{1.0f, 1.0f, 0.0f, 1.0f },
+		{1.0f, 1.0f, 1.0f , 1.0f},
+		{0.0f, 1.0f, 1.0f , 1.0f },
+		{0.0f, 1.0f, 0.0f , 1.0f},
+		{0.0f, 0.0f, 1.0f, 1.0f }
+	}
+};
+
+class BoxApp : public D3DApp
 {
 public:
-	TriangleAPP(HINSTANCE hInstance);
-	TriangleAPP(const TriangleAPP& rhs) = delete;
-	TriangleAPP& operator=(const TriangleAPP& rhs) = delete;
-	~TriangleAPP();
+	BoxApp(HINSTANCE hInstance);
+	BoxApp(const BoxApp& rhs) = delete;
+	BoxApp& operator=(const BoxApp& rhs) = delete;
+	~BoxApp();
 
 	virtual bool Initialize()override;
 
@@ -38,6 +60,11 @@ private:
 	virtual void OnResize()override;
 	virtual void Update(const GameTimer& gt)override;
 	virtual void Draw(const GameTimer& gt)override;
+
+	//step4
+	virtual void OnMouseDown(WPARAM btnState, int x, int y)override;
+	virtual void OnMouseUp(WPARAM btnState, int x, int y)override;
+	virtual void OnMouseMove(WPARAM btnState, int x, int y)override;
 
 
 	void BuildDescriptorHeaps();
@@ -55,6 +82,7 @@ private:
 	ComPtr<ID3D12RootSignature> mRootSignature = nullptr;
 	ComPtr<ID3D12DescriptorHeap> mCbvHeap = nullptr;
 	std::unique_ptr<UploadBuffer<ObjectConstants>> mObjectCB = nullptr;
+	std::unique_ptr<UploadBuffer<ObjectConstant2>> mObjectCB2 = nullptr;
 
 	//The ID3DBlob interface is used to return data of arbitrary length. It's contained in D3dcompiler.lib 
 	ComPtr<ID3DBlob> mvsByteCode = nullptr;
@@ -73,10 +101,6 @@ private:
 	float mPhi = XM_PIDIV4;
 	float mRadius = 5.0f;
 
-	//step1
-	float mTheta2 = 0.0f;
-	//
-
 	POINT mLastMousePos;
 
 };
@@ -91,7 +115,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE prevInstance,
 
 	try
 	{
-		TriangleAPP theApp(hInstance);
+		BoxApp theApp(hInstance);
 		if (!theApp.Initialize())
 			return 0;
 
@@ -104,16 +128,16 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE prevInstance,
 	}
 }
 
-TriangleAPP::TriangleAPP(HINSTANCE hInstance)
+BoxApp::BoxApp(HINSTANCE hInstance)
 	: D3DApp(hInstance)
 {
 }
 
-TriangleAPP::~TriangleAPP()
+BoxApp::~BoxApp()
 {
 }
 
-bool TriangleAPP::Initialize()
+bool BoxApp::Initialize()
 {
 	if (!D3DApp::Initialize())
 		return false;
@@ -141,18 +165,19 @@ bool TriangleAPP::Initialize()
 	return true;
 }
 
-void TriangleAPP::OnResize()
+void BoxApp::OnResize()
 {
 	D3DApp::OnResize();
 
-		// The window resized, so update the aspect ratio and recompute the projection matrix.
-	XMMATRIX P = XMMatrixPerspectiveFovLH(0.10f * MathHelper::Pi, AspectRatio(), 1.0f, 1000.0f);
+	// The window resized, so update the aspect ratio and recompute the projection matrix.
+//step3: change FOV angle to 45 degree
+	XMMATRIX P = XMMatrixPerspectiveFovLH(0.25f * MathHelper::Pi, AspectRatio(), 1.0f, 1000.0f);
 	XMStoreFloat4x4(&mProj, P);
 }
 
 
 
-void TriangleAPP::Update(const GameTimer& gt)
+void BoxApp::Update(const GameTimer& gt)
 {
 
 	// Convert Spherical to Cartesian coordinates.
@@ -168,13 +193,11 @@ void TriangleAPP::Update(const GameTimer& gt)
 	XMMATRIX view = XMMatrixLookAtLH(pos, target, up);
 	XMStoreFloat4x4(&mView, view);
 
-
-
 	XMMATRIX world = XMLoadFloat4x4(&mWorld);
 
 	//step2 a rotation around z axis
-	mTheta2 += 0.1f;
-	world = XMMatrixRotationZ((-XM_PI+ mTheta2) / 6.0f);
+	//mTheta2 += 0.1f;
+	//world = XMMatrixRotationZ((-XM_PI+ mTheta2) / 6.0f);
 	//
 
 	XMMATRIX proj = XMLoadFloat4x4(&mProj);
@@ -185,9 +208,15 @@ void TriangleAPP::Update(const GameTimer& gt)
 	XMStoreFloat4x4(&objConstants.WorldViewProj, XMMatrixTranspose(worldViewProj));
 	mObjectCB->CopyData(0, objConstants);
 
+
+
+	//hooman: don't forget to upload your color data
+
+	mObjectCB2->CopyData(0, cb2);
+
 }
 
-void TriangleAPP::Draw(const GameTimer& gt)
+void BoxApp::Draw(const GameTimer& gt)
 {
 	// Reuse the memory associated with command recording.
 	// We can only reset when the associated command lists have finished execution on the GPU.
@@ -198,47 +227,41 @@ void TriangleAPP::Draw(const GameTimer& gt)
 	ThrowIfFailed(mCommandList->Reset(mDirectCmdListAlloc.Get(), mPSO.Get()));
 
 	//You cannot render without a view port. 
-	//viewport maps a NDC coordinate (like 0,1) to Render Target (400,0) assuming screen space 800X800 pixels.
-	//also you use viewport to map to subportion of the render target (like top left rectangle of your screen)
-	//also notice viewport(s), because you can have an array of view ports! & is poiting to the address of the first viewport
 	mCommandList->RSSetViewports(1, &mScreenViewport);
 
 	//Each scissor rectangle in the array corresponds to a viewport in an array of viewports
 	mCommandList->RSSetScissorRects(1, &mScissorRect);
 
 	// Indicate a state transition on the resource usage.
-	//Specifies the state of a resource regarding how the resource is being used.
-	//D3D12_RESOURCE_STATE_PRESENT same as D3D12_RESOURCE_STATE_COMMON
-	//Applications should only transition to this state for accessing a resource across different graphics engine types.
-	//Specifically, a resource must be in the COMMON state before being used on a COPY queue
 	mCommandList->ResourceBarrier(1, &CD3DX12_RESOURCE_BARRIER::Transition(CurrentBackBuffer(),
 		D3D12_RESOURCE_STATE_PRESENT, D3D12_RESOURCE_STATE_RENDER_TARGET));
 
 
 	// Clear the back buffer and depth buffer.
 	mCommandList->ClearRenderTargetView(CurrentBackBufferView(), Colors::LightSteelBlue, 0, nullptr);
-
-	//step6: let's get ready for 3D
 	mCommandList->ClearDepthStencilView(DepthStencilView(), D3D12_CLEAR_FLAG_DEPTH | D3D12_CLEAR_FLAG_STENCIL, 1.0f, 0, 0, nullptr);
 	mCommandList->OMSetRenderTargets(1, &CurrentBackBufferView(), true, &DepthStencilView());
-	//mCommandList->OMSetRenderTargets(1, &CurrentBackBufferView(), true, nullptr);
-	//
 
 	ID3D12DescriptorHeap* descriptorHeaps[] = { mCbvHeap.Get() };
 	mCommandList->SetDescriptorHeaps(_countof(descriptorHeaps), descriptorHeaps);
 
 	mCommandList->SetGraphicsRootSignature(mRootSignature.Get());
 
+	//hooman: 
+	int FaceColorCbvIndex = 0;
+	auto FaceColorCbvHandle = CD3DX12_GPU_DESCRIPTOR_HANDLE(mCbvHeap->GetGPUDescriptorHandleForHeapStart());
+	FaceColorCbvHandle.Offset(FaceColorCbvIndex, mCbvSrvUavDescriptorSize);
+	mCommandList->SetGraphicsRootDescriptorTable(1, FaceColorCbvHandle);
+	//
+
 	mCommandList->IASetVertexBuffers(0, 1, &mBoxGeo->VertexBufferView());
 	mCommandList->IASetIndexBuffer(&mBoxGeo->IndexBufferView());   //bind it to the pipeline
 
 	mCommandList->IASetPrimitiveTopology(D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
 
-	//step12 bind the constant buffer to the pipeline
 	mCommandList->SetGraphicsRootDescriptorTable(0, mCbvHeap->GetGPUDescriptorHandleForHeapStart());
-	// if you compile the code at this point, there is no build problem. but it doesn't do anything, because the shader is not making a use of it.
 
-	mCommandList->DrawIndexedInstanced(mBoxGeo->DrawArgs["triangle"].IndexCount, 1, 0, 0, 0);
+	mCommandList->DrawIndexedInstanced(mBoxGeo->DrawArgs["box"].IndexCount, 1, 0, 0, 0);
 
 
 	// Indicate a state transition on the resource usage.
@@ -267,9 +290,9 @@ void TriangleAPP::Draw(const GameTimer& gt)
 
 
 
-void TriangleAPP::BuildRootSignature()
+void BoxApp::BuildRootSignature()
 {
- 
+
 	// Shader programs typically require resources as input (constant buffers,
 	// textures, samplers).  The root signature defines the resources the shader
 	// programs expect.  If we think of the shader programs as a function, and
@@ -277,16 +300,23 @@ void TriangleAPP::BuildRootSignature()
 	// thought of as defining the function signature.  
 
 	// Root parameter can be a table, root descriptor or root constants.
-	CD3DX12_ROOT_PARAMETER slotRootParameter[1];
+	//Hooman: we can add two ranges with two parameters
+	CD3DX12_DESCRIPTOR_RANGE cbvTable[2] = {};
+	cbvTable[0].Init(D3D12_DESCRIPTOR_RANGE_TYPE_CBV, 1, 0);
+	cbvTable[1].Init(D3D12_DESCRIPTOR_RANGE_TYPE_CBV, 1, 1);
 
-	// Create a single descriptor table of CBVs.
-	CD3DX12_DESCRIPTOR_RANGE cbvTable;
-	cbvTable.Init(D3D12_DESCRIPTOR_RANGE_TYPE_CBV, 1, 0);
-	slotRootParameter[0].InitAsDescriptorTable(1, &cbvTable);
+	// Root parameter can be a table, root descriptor or root constants.
+	CD3DX12_ROOT_PARAMETER slotRootParameter[2];
+
+	// Create root CBVs.
+	slotRootParameter[0].InitAsDescriptorTable(1, &cbvTable[0]);
+	slotRootParameter[1].InitAsDescriptorTable(1, &cbvTable[1]);
+
 
 	// A root signature is an array of root parameters.
-	CD3DX12_ROOT_SIGNATURE_DESC rootSigDesc(1, slotRootParameter, 0, nullptr,
+	CD3DX12_ROOT_SIGNATURE_DESC rootSigDesc(2, slotRootParameter, 0, nullptr,
 		D3D12_ROOT_SIGNATURE_FLAG_ALLOW_INPUT_ASSEMBLER_INPUT_LAYOUT);
+
 
 
 	// create a root signature with no slot for now!
@@ -308,39 +338,72 @@ void TriangleAPP::BuildRootSignature()
 }
 
 
-
-
-void TriangleAPP::BuildShadersAndInputLayout()
+void BoxApp::BuildShadersAndInputLayout()
 {
 	HRESULT hr = S_OK;
 
 	//when you are adding new shader, make sure you right click, and set shader type and shader model in HLSL compiler 
 	//step3: refactoring the vertex shader to use structs for both ins and outs parameters
 	//note that I changed "main" to PS and VS
-	mvsByteCode = d3dUtil::CompileShader(L"Shaders\\VS3.hlsl", nullptr, "VS", "vs_5_0");
-	mpsByteCode = d3dUtil::CompileShader(L"Shaders\\PS3.hlsl", nullptr, "PS", "ps_5_0");
+	mvsByteCode = d3dUtil::CompileShader(L"Shaders\\VS4.hlsl", nullptr, "VS", "vs_5_0");
+	mpsByteCode = d3dUtil::CompileShader(L"Shaders\\PS4.hlsl", nullptr, "PS", "ps_5_0");
 
 	mInputLayout =
 	{
 		{ "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0 },
-		{ "COLOR", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 12, D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0 },
+		/*{ "COLOR", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 12, D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0 },*/
 	};
 }
 
-void TriangleAPP::BuildTriangleGeometry()
+void BoxApp::BuildTriangleGeometry()
 {
-	std::array<Vertex, 3> vertices =
+	//step1
+	std::array<Vertex, 8> vertices =
 	{
-				Vertex({ XMFLOAT3(-0.5f, -0.5f, 0.0f), XMFLOAT4(Colors::Red)}),
-				Vertex({ XMFLOAT3(+0.0f, +0.5f, 0.0f) , XMFLOAT4(Colors::Green) }),
-				Vertex({ XMFLOAT3(+0.5f, -0.5f, 0.0f) , XMFLOAT4(Colors::Blue)}),
+		//Vertex({ XMFLOAT3(-1.0f, -1.0f, -1.0f), XMFLOAT4(Colors::White) }),
+		//Vertex({ XMFLOAT3(-1.0f, +1.0f, -1.0f), XMFLOAT4(Colors::Black) }),
+		//Vertex({ XMFLOAT3(+1.0f, +1.0f, -1.0f), XMFLOAT4(Colors::Red) }),
+		//Vertex({ XMFLOAT3(+1.0f, -1.0f, -1.0f), XMFLOAT4(Colors::Green) }),
+		//Vertex({ XMFLOAT3(-1.0f, -1.0f, +1.0f), XMFLOAT4(Colors::Blue) }),
+		//Vertex({ XMFLOAT3(-1.0f, +1.0f, +1.0f), XMFLOAT4(Colors::Yellow) }),
+		//Vertex({ XMFLOAT3(+1.0f, +1.0f, +1.0f), XMFLOAT4(Colors::Cyan) }),
+		//Vertex({ XMFLOAT3(+1.0f, -1.0f, +1.0f), XMFLOAT4(Colors::Magenta) })
+
+		Vertex({ XMFLOAT3(-1.0f, -1.0f, -1.0f) }),
+		Vertex({ XMFLOAT3(-1.0f, +1.0f, -1.0f) }),
+		Vertex({ XMFLOAT3(+1.0f, +1.0f, -1.0f) }),
+		Vertex({ XMFLOAT3(+1.0f, -1.0f, -1.0f) }),
+		Vertex({ XMFLOAT3(-1.0f, -1.0f, +1.0f) }),
+		Vertex({ XMFLOAT3(-1.0f, +1.0f, +1.0f) }),
+		Vertex({ XMFLOAT3(+1.0f, +1.0f, +1.0f) }),
+		Vertex({ XMFLOAT3(+1.0f, -1.0f, +1.0f) })
 	};
 
-
-	std::array<std::uint16_t, 3> indices =
+	std::array<std::uint16_t, 36> indices =
 	{
-
+		// front face
 		0, 1, 2,
+		0, 2, 3,
+
+		// back face
+		4, 6, 5,
+		4, 7, 6,
+
+		// left face
+		4, 5, 1,
+		4, 1, 0,
+
+		// right face
+		3, 2, 6,
+		3, 6, 7,
+
+		// top face
+		1, 5, 6,
+		1, 6, 2,
+
+		// bottom face
+		4, 0, 3,
+		4, 3, 7
 	};
 
 
@@ -349,7 +412,7 @@ void TriangleAPP::BuildTriangleGeometry()
 	const UINT ibByteSize = (UINT)indices.size() * sizeof(std::uint16_t);
 
 	mBoxGeo = std::make_unique<MeshGeometry>();
-	mBoxGeo->Name = "triangleGeo";
+	mBoxGeo->Name = "boxGeo";
 
 	ThrowIfFailed(D3DCreateBlob(vbByteSize, &mBoxGeo->VertexBufferCPU));
 	CopyMemory(mBoxGeo->VertexBufferCPU->GetBufferPointer(), vertices.data(), vbByteSize);
@@ -373,12 +436,12 @@ void TriangleAPP::BuildTriangleGeometry()
 	submesh.StartIndexLocation = 0;
 	submesh.BaseVertexLocation = 0;
 
-	mBoxGeo->DrawArgs["triangle"] = submesh;
+	mBoxGeo->DrawArgs["box"] = submesh;
 
 
 }
 
-void TriangleAPP::BuildPSO()
+void BoxApp::BuildPSO()
 {
 	D3D12_GRAPHICS_PIPELINE_STATE_DESC psoDesc;
 	ZeroMemory(&psoDesc, sizeof(D3D12_GRAPHICS_PIPELINE_STATE_DESC));
@@ -386,8 +449,6 @@ void TriangleAPP::BuildPSO()
 	//bind the input layout to pipeline
 	psoDesc.InputLayout = { mInputLayout.data(), (UINT)mInputLayout.size() };
 	psoDesc.pRootSignature = mRootSignature.Get();
-
-	//in old days, we use to call create vertex shader, now we hook it up to the PSO!
 	psoDesc.VS =
 	{
 		reinterpret_cast<BYTE*>(mvsByteCode->GetBufferPointer()),
@@ -401,57 +462,123 @@ void TriangleAPP::BuildPSO()
 	psoDesc.RasterizerState = CD3DX12_RASTERIZER_DESC(D3D12_DEFAULT);
 	psoDesc.BlendState = CD3DX12_BLEND_DESC(D3D12_DEFAULT);
 
-	//step8
 	psoDesc.DepthStencilState = CD3DX12_DEPTH_STENCIL_DESC(D3D12_DEFAULT);
-	//psoDesc.DepthStencilState.DepthEnable = FALSE;
-	//psoDesc.DepthStencilState.StencilEnable = FALSE;
-
-
 	psoDesc.SampleMask = UINT_MAX;
 	psoDesc.PrimitiveTopologyType = D3D12_PRIMITIVE_TOPOLOGY_TYPE_TRIANGLE;
 
 	psoDesc.NumRenderTargets = 1;
 	psoDesc.RTVFormats[0] = mBackBufferFormat;  //DXGI_FORMAT_R8G8B8A8_UNORM;
-
-	//step9
-	//	psoDesc.SampleDesc.Count = 1;
 	psoDesc.SampleDesc.Count = m4xMsaaState ? 4 : 1;
 	psoDesc.SampleDesc.Quality = m4xMsaaState ? (m4xMsaaQuality - 1) : 0;
 	psoDesc.DSVFormat = mDepthStencilFormat;
-	//end
+
 	ThrowIfFailed(md3dDevice->CreateGraphicsPipelineState(&psoDesc, IID_PPV_ARGS(&mPSO)));
 }
 
 
-void TriangleAPP::BuildDescriptorHeaps()
+void BoxApp::BuildDescriptorHeaps()
 {
 	D3D12_DESCRIPTOR_HEAP_DESC cbvHeapDesc;
-	cbvHeapDesc.NumDescriptors = 1;
+	cbvHeapDesc.NumDescriptors = 2; 
 	cbvHeapDesc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV;
 	cbvHeapDesc.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_SHADER_VISIBLE;
 	cbvHeapDesc.NodeMask = 0;
-	ThrowIfFailed(md3dDevice->CreateDescriptorHeap(&cbvHeapDesc,
-		IID_PPV_ARGS(&mCbvHeap)));
+	ThrowIfFailed(md3dDevice->CreateDescriptorHeap(&cbvHeapDesc, IID_PPV_ARGS(&mCbvHeap)));
+	mCbvHeap->SetName(L"Constant Buffer View Descriptor Heap");
 }
 
-void TriangleAPP::BuildConstantBuffers()
+void BoxApp::BuildConstantBuffers()
 {
-	mObjectCB = std::make_unique<UploadBuffer<ObjectConstants>>(md3dDevice.Get(), 1, true);
 
-	UINT objCBByteSize = d3dUtil::CalcConstantBufferByteSize(sizeof(ObjectConstants));
 
-	D3D12_GPU_VIRTUAL_ADDRESS cbAddress = mObjectCB->Resource()->GetGPUVirtualAddress();
-	// Offset to the ith object constant buffer in the buffer.
-	int boxCBufIndex = 0;
-	cbAddress += boxCBufIndex * objCBByteSize;
 
-	D3D12_CONSTANT_BUFFER_VIEW_DESC cbvDesc;
-	cbvDesc.BufferLocation = cbAddress;
-	cbvDesc.SizeInBytes = d3dUtil::CalcConstantBufferByteSize(sizeof(ObjectConstants));
 
-	md3dDevice->CreateConstantBufferView(
-		&cbvDesc,
-		mCbvHeap->GetCPUDescriptorHandleForHeapStart());
+	D3D12_CONSTANT_BUFFER_VIEW_DESC cbvDesc[2] = {};
+
+		mObjectCB = std::make_unique<UploadBuffer<ObjectConstants>>(md3dDevice.Get(), 1, true);
+
+		UINT objCBByteSize = d3dUtil::CalcConstantBufferByteSize(sizeof(ObjectConstants));
+
+		D3D12_GPU_VIRTUAL_ADDRESS cbAddress = mObjectCB->Resource()->GetGPUVirtualAddress();
+		// Offset to the ith object constant buffer in the buffer.
+		int boxCBufIndex = 0;
+		cbAddress += boxCBufIndex * objCBByteSize;
+
+
+		cbvDesc[0].BufferLocation = cbAddress;
+		cbvDesc[0].SizeInBytes = d3dUtil::CalcConstantBufferByteSize(sizeof(ObjectConstants));
+
+
+
+		mObjectCB2 = std::make_unique<UploadBuffer<ObjectConstant2>>(md3dDevice.Get(), 1, true);
+
+		UINT objCB2ByteSize = d3dUtil::CalcConstantBufferByteSize(sizeof(ObjectConstant2));
+
+		D3D12_GPU_VIRTUAL_ADDRESS cb2Address = mObjectCB2->Resource()->GetGPUVirtualAddress();
+		// Offset to the ith object constant buffer in the buffer.
+		int mFaceColorCbvOffset = 0;
+		cb2Address += mFaceColorCbvOffset * objCB2ByteSize;
+
+		// Offset to the facecolor cbv in the descriptor heap.
+
+
+		cbvDesc[1].BufferLocation = cb2Address;
+		cbvDesc[1].SizeInBytes = d3dUtil::CalcConstantBufferByteSize(sizeof(ObjectConstant2));
+
+
+		CD3DX12_CPU_DESCRIPTOR_HANDLE cbvHandle0(mCbvHeap->GetCPUDescriptorHandleForHeapStart(), 0, 0);
+		md3dDevice->CreateConstantBufferView(&cbvDesc[0], cbvHandle0);
+
+		CD3DX12_CPU_DESCRIPTOR_HANDLE cbvHandle1(mCbvHeap->GetCPUDescriptorHandleForHeapStart(), md3dDevice->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV), 1);
+		md3dDevice->CreateConstantBufferView(&cbvDesc[1], cbvHandle1);
+
 }
 
 
+//step5
+
+
+void BoxApp::OnMouseDown(WPARAM btnState, int x, int y)
+{
+	mLastMousePos.x = x;
+	mLastMousePos.y = y;
+
+	SetCapture(mhMainWnd);
+}
+
+void BoxApp::OnMouseUp(WPARAM btnState, int x, int y)
+{
+	ReleaseCapture();
+}
+
+void BoxApp::OnMouseMove(WPARAM btnState, int x, int y)
+{
+	if ((btnState & MK_LBUTTON) != 0)
+	{
+		// Make each pixel correspond to a quarter of a degree.
+		float dx = XMConvertToRadians(0.25f * static_cast<float>(x - mLastMousePos.x));
+		float dy = XMConvertToRadians(0.25f * static_cast<float>(y - mLastMousePos.y));
+
+		// Update angles based on input to orbit camera around box.
+		mTheta += dx;
+		mPhi += dy;
+
+		// Restrict the angle mPhi.
+		mPhi = MathHelper::Clamp(mPhi, 0.1f, MathHelper::Pi - 0.1f);
+	}
+	else if ((btnState & MK_RBUTTON) != 0)
+	{
+		// Make each pixel correspond to 0.005 unit in the scene.
+		float dx = 0.005f * static_cast<float>(x - mLastMousePos.x);
+		float dy = 0.005f * static_cast<float>(y - mLastMousePos.y);
+
+		// Update the camera radius based on input.
+		mRadius += dx - dy;
+
+		// Restrict the radius.
+		mRadius = MathHelper::Clamp(mRadius, 3.0f, 15.0f);
+	}
+
+	mLastMousePos.x = x;
+	mLastMousePos.y = y;
+}
