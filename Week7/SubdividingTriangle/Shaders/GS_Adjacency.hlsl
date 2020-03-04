6bec361a86451debe3c934d6d58eaf5a16d202d4
hsalamat <hooman_salamat@yahoo.com>
2020-03-04 15:13:32 -0500

    Update GS_Adjacency.hlsl

--- Week7/SubdividingTriangle/Shaders/GS_Adjacency.hlsl
+++ Week7/SubdividingTriangle/Shaders/GS_Adjacency.hlsl
@@ -29,7 +29,7 @@ void GS(triangleadj VertexOut gin[6], //PrimitiveType InputVertexType InputName[
 
     GeoOut gout[6];
     [unroll]
-    for (uint i = 0; i < 6; i=i+2)
+    for (uint i = 0; i < 6; i=i+2)    //0,2,4 triangle
     {
         // Transform to homogeneous clip space.
         gout[i].PosH = mul(float4(gin[i].PosW, 1.0f), gWorldViewProj);
@@ -39,8 +39,8 @@ void GS(triangleadj VertexOut gin[6], //PrimitiveType InputVertexType InputName[
 
     //Remember For line and triangle(triStream) GS always outputs a line strip or a triangle strip! If you wanted to output triangle list, you need to call RestartStrip() every time three vertices appended to output stream.
     triStream.RestartStrip();
-
-    for (uint i = 0; i < 3; i ++)
+    [unroll]
+    for ( i = 0; i < 3; i ++) //0,1,2 triangle
     {
         // Transform to homogeneous clip space.
         gout[i].PosH = mul(float4(gin[i].PosW, 1.0f), gWorldViewProj);
@@ -51,8 +51,8 @@ void GS(triangleadj VertexOut gin[6], //PrimitiveType InputVertexType InputName[
 
 
     triStream.RestartStrip();
-
-    for (uint i = 2; i < 5; i++)
+    [unroll]
+    for ( i = 2; i < 5; i++) //2,3,4 triangle
     {
         // Transform to homogeneous clip space.
         gout[i].PosH = mul(float4(gin[i].PosW, 1.0f), gWorldViewProj);
@@ -60,5 +60,21 @@ void GS(triangleadj VertexOut gin[6], //PrimitiveType InputVertexType InputName[
         triStream.Append(gout[i]);
     }
     
+    triStream.RestartStrip();
+
+    //0,4,5 triangle
+
+    gout[0].PosH = mul(float4(gin[0].PosW, 1.0f), gWorldViewProj);
+    gout[0].Color = gin[0].Color;
+    triStream.Append(gout[0]);
+
+    [unroll]
+    for ( i = 4; i < 6; i++)
+    {
+        gout[i].PosH = mul(float4(gin[i].PosW, 1.0f), gWorldViewProj);
+        gout[i].Color = gin[i].Color;
+        triStream.Append(gout[i]);
+    }
+
 
 }
