9174914359451e8d9c6fd68d4a9ef5276b7c950d
hsalamat <2hooman.salamat@gmail.com>
2024-01-15 11:01:42 -0500

    week2 updates

---
+++ Shaders/color.hlsl
@@ -0,0 +1,42 @@
+//***************************************************************************************
+// Transforms and colors geometry.
+//***************************************************************************************
+
+cbuffer cbPerObject : register(b0)
+{
+	float4x4 gWorldViewProj; 
+};
+
+struct VertexIn
+{
+	float3 PosL  : POSITION;
+    float4 Color : COLOR;
+};
+
+struct VertexOut
+{
+	float4 PosH  : SV_POSITION;
+    float4 Color : COLOR;
+};
+
+VertexOut VS(VertexIn vin)
+{
+	VertexOut vout;
+	
+	// Transform to homogeneous clip space.
+	vout.PosH = mul(float4(vin.PosL, 1.0f), gWorldViewProj);
+
+	// Just pass vertex color into the pixel shader.
+    vout.Color = vin.Color;
+    
+    return vout;
+}
+
+float4 PS(VertexOut pin) : SV_Target
+{
+	return pin.Color;
+	//try this
+	//return float4(0.0f,pin.Color[0],sin(pin.Color[1] * 90),1.0f);
+}
+
+
---
+++ Week2/Week2-8-Console-COM-Demo/1_Interface_Demo.cpp
@@ -0,0 +1,98 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+//An interface describes the behavior or capabilities of a C++ class without committing to a particular implementation of that class.
+
+//The C++ interfaces are implemented using abstract classes and these abstract classes should not be confused with data abstraction which is a concept of keeping implementation details separate from associated data.
+
+//A class is made abstract by declaring at least one of its functions as pure virtual function.A pure virtual function is specified by placing "= 0" in its declaration as follows −
+
+class IDrawable
+{
+public:
+	virtual void Draw() = 0;
+};
+
+//Designing Strategy
+//An object - oriented system might use an abstract base class to provide a common and standardized interface appropriate for all the external applications.
+// Then, through inheritance from that abstract base class, derived classes are formed that operate similarly.
+
+class Shape : public IDrawable
+{
+public:
+	virtual void Draw() {
+		cout << "Draw Shape: Rasterize a set of lines " << endl;
+	}
+	string name;
+private:
+	
+};
+
+Shape* CreateShape()
+{
+	Shape triangleShape;
+	triangleShape.name = "MyShape";
+	return &triangleShape;
+}
+
+class Bitmap : public IDrawable
+{
+public:
+	virtual void Draw() {
+		cout << "Draw Bitmap: Blit an array of pixels " << endl;
+	}
+	string name;
+private:
+};
+
+Bitmap* CreateBitmap()
+{
+	Bitmap bitmap;
+	bitmap.name = "MyPicture.bmp";
+	return &bitmap;
+}
+
+//Becarefull using this function: IDrawble is just an interface!
+// it is important to remember that interfaces are not objects. They are collections of methods that objects must implement.
+void DrawSomeShapes(IDrawable** drawableArray, size_t count)
+{
+	for (int i = 0; i < count; i++)
+	{
+		drawableArray[i]->Draw();
+	}
+}
+int main(void)
+{
+	//you can not create an instance of an IDrwable!
+	//IDrawable draw;
+	//draw.Draw();
+
+	//A Program using this graphics library would manipulate Shape or Bitmap objects through IDrawable pointers, rather than Shape or Bitmap directly
+	IDrawable* drawableArray[5];
+
+	IDrawable* sDrawable = CreateShape();
+	if (sDrawable)
+	{
+		sDrawable->Draw();
+	}
+
+
+	IDrawable* bDrawable = CreateBitmap();
+	if (bDrawable)
+	{
+		bDrawable->Draw();
+	}
+
+
+	drawableArray[0] = CreateShape();
+	drawableArray[1] = CreateShape();
+	drawableArray[2] = CreateBitmap();
+	DrawSomeShapes(drawableArray, 3);
+
+
+
+
+	return 0;
+}
